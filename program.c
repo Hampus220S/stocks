53b29d04496c407a17dfc07922fc0d6be1251880
Hampus Fridholm <hampus.fridholm@gmail.com>
2025-03-19 22:04:33 +0100

    Created tui_create and tui_delete. Created colors _on and _off functions.

--- program.c
+++ program.c
@@ -26,6 +26,68 @@ int main(int argc, char* argv[])
 
   info_print("Initialized TUI");
 
+
+  tui_t* tui = tui_create(NULL);
+
+  if (!tui)
+  {
+    info_print("Failed to create TUI");
+
+    tui_quit();
+
+    debug_file_close();
+
+    return 2;
+  }
+
+  info_print("Created TUI");
+
+
+  tui->is_running = true;
+
+  clear();
+
+  tui_render(tui);
+
+  int index = 0;
+
+  tui_color_on(tui, TUI_COLOR_WHITE, TUI_COLOR_GREEN);
+
+  int key;
+
+  while (tui->is_running && (key = wgetch(stdscr)))
+  {
+    erase();
+
+    tui_color_t fg_color = index + 1;
+
+    index = (index + 1) % 8;
+
+    tui_color_on(tui, fg_color, TUI_COLOR_NONE);
+
+    mvprintw(1, 1, "KEY: %d", key);
+
+    tui_color_off(tui, fg_color, TUI_COLOR_NONE);
+
+    if (key == KEY_CTRLS)
+    {
+      tui->is_running = false;
+      break;
+    }
+
+    tui_event(tui, key);
+
+    tui_render(tui);
+  }
+
+  tui_color_off(tui, TUI_COLOR_WHITE, TUI_COLOR_GREEN);
+
+
+  tui_delete(&tui);
+
+  info_print("Deleted TUI");
+
+
   tui_quit();
 
   info_print("Quitted TUI");
--- tui.h
+++ tui.h
@@ -16,7 +16,20 @@
 #include <stdint.h>
 
 /*
- * Declarations of TUI structs
+ * Definitions of keys
+ */
+
+#define KEY_CTRLC  3
+#define KEY_CTRLZ 26
+#define KEY_ESC   27
+#define KEY_CTRLS 19
+#define KEY_CTRLH  8
+#define KEY_CTRLD  4
+#define KEY_ENTR  10
+#define KEY_TAB    9
+
+/*
+ * Declarations of tui structs
  */
 
 typedef struct tui_t tui_t;
@@ -149,15 +162,15 @@ typedef enum tui_align_t
  */
 typedef struct tui_window_input_t
 {
-  tui_window_t* head;
-  char*         buffer;
-  size_t        buffer_size;
-  size_t        buffer_len;
-  size_t        cursor;
-  size_t        scroll;
-  bool          is_secret;
-  bool          is_hidden;
-  tui_pos_t     pos;
+  tui_window_t head;
+  char*        buffer;
+  size_t       buffer_size;
+  size_t       buffer_len;
+  size_t       cursor;
+  size_t       scroll;
+  bool         is_secret;
+  bool         is_hidden;
+  tui_pos_t    pos;
 } tui_window_input_t;
 
 /*
@@ -165,11 +178,11 @@ typedef struct tui_window_input_t
  */
 typedef struct tui_window_text_t
 {
-  tui_window_t* head;
-  char*         string;
-  char*         text;
-  tui_pos_t     pos;
-  tui_align_t   align;
+  tui_window_t head;
+  char*        string;
+  char*        text;
+  tui_pos_t    pos;
+  tui_align_t  align;
 } tui_window_text_t;
 
 /*
@@ -177,11 +190,12 @@ typedef struct tui_window_text_t
  */
 typedef struct tui_window_parent_t
 {
-  tui_window_t* children;
-  size_t        child_count;
-  bool          is_vertical;
-  tui_pos_t     pos;
-  tui_align_t   align;
+  tui_window_t   head;
+  tui_window_t** children;
+  size_t         child_count;
+  bool           is_vertical;
+  tui_pos_t      pos;
+  tui_align_t    align;
 } tui_window_parent_t;
 
 /*
@@ -197,7 +211,7 @@ typedef struct tui_menu_t
 } tui_menu_t;
 
 /*
- * TUI struct
+ * Tui struct
  */
 typedef struct tui_t
 {
@@ -211,6 +225,7 @@ typedef struct tui_t
   size_t         tab_window_count;
   tui_menu_t*    menu;
   tui_window_t*  window;
+  short          color;
   tui_event_t    event;
   bool           is_running;
 } tui_t;
@@ -226,8 +241,95 @@ typedef struct tui_t
 
 #include "debug.h"
 
+short TUI_COLORS[] =
+{
+  -1,
+  COLOR_BLACK,
+  COLOR_RED,
+  COLOR_GREEN,
+  COLOR_YELLOW,
+  COLOR_BLUE,
+  COLOR_MAGENTA,
+  COLOR_CYAN,
+  COLOR_WHITE
+};
+
 /*
- * Init tui (ncurses)
+ * Turn on forground and background color
+ *
+ * In case of transparent colors (TUI_COLOR_NONE),
+ * the last color is used
+ *
+ * ncurses colors and tui colors differ by 1
+ */
+void tui_color_on(tui_t* tui, tui_color_t fg, tui_color_t bg)
+{
+  short last_fg, last_bg;
+
+  if (pair_content(tui->color, &last_fg, &last_bg) == OK)
+  {
+    if (fg == TUI_COLOR_NONE)
+    {
+      fg = last_fg + 1;
+    }
+
+    if (bg == TUI_COLOR_NONE)
+    {
+      bg = last_bg + 1;
+    }
+  }
+
+  short color = fg * 9 + bg;
+
+  tui->color = color;
+
+  attron(COLOR_PAIR(color));
+}
+
+/*
+ * Turn off forground and background color
+ */
+void tui_color_off(tui_t* tui, tui_color_t fg, tui_color_t bg)
+{
+  short last_fg, last_bg;
+
+  if (pair_content(tui->color, &last_fg, &last_bg) == OK)
+  {
+    if (fg == TUI_COLOR_NONE)
+    {
+      fg = last_fg + 1;
+    }
+
+    if (bg == TUI_COLOR_NONE)
+    {
+      bg = last_bg + 1;
+    }
+  }
+
+  attroff(COLOR_PAIR(fg * 9 + bg));
+}
+
+/*
+ * Initialize tui colors
+ */
+void tui_colors_init(void)
+{
+  for (size_t fg_index = 0; fg_index < 9; fg_index++)
+  {
+    for (size_t bg_index = 0; bg_index < 9; bg_index++)
+    {
+      size_t index = fg_index * 9 + bg_index;
+
+      short fg_color = TUI_COLORS[fg_index];
+      short bg_color = TUI_COLORS[bg_index];
+
+      init_pair(index, fg_color, bg_color);
+    }
+  }
+}
+
+/*
+ * Initialize tui (ncurses)
  */
 int tui_init(void)
 {
@@ -243,6 +345,13 @@ int tui_init(void)
     return 1;
   }
 
+  use_default_colors();
+
+  tui_colors_init();
+
+  clear();
+  refresh();
+
   return 0;
 }
 
@@ -256,6 +365,209 @@ void tui_quit(void)
   refresh();
 
   endwin();
+}
+
+/*
+ * Create ncurses WINDOW* for tui_window_t
+ */
+static inline WINDOW* tui_ncurses_window_create(int w, int h, int x, int y)
+{
+  WINDOW* window = newwin(h, w, y, x);
+
+  if (!window)
+  {
+    return NULL;
+  }
+
+  keypad(window, TRUE);
+
+  return window;
+}
+
+/*
+ * Resize ncurses WINDOW*
+ */
+static inline void tui_ncurses_window_resize(WINDOW* window, int w, int h, int x, int y)
+{
+  wresize(window, h, w);
+
+  mvwin(window, y, x);
+}
+
+/*
+ * Delete ncurses WINDOW*
+ */
+static inline void tui_ncurses_window_free(WINDOW** window)
+{
+  if (!window || !(*window)) return;
+
+  wclear(*window);
+
+  wrefresh(*window);
+
+  delwin(*window);
+
+  *window = NULL;
+}
+
+/*
+ * Create tui struct
+ */
+tui_t* tui_create(tui_event_t event)
+{
+  tui_t* tui = malloc(sizeof(tui_t));
+
+  if (!tui)
+  {
+    return NULL;
+  }
+
+  memset(tui, 0, sizeof(tui_t));
+
+  *tui = (tui_t)
+  {
+    .w     = getmaxx(stdscr),
+    .h     = getmaxy(stdscr),
+    .event = event
+  };
+
+  return tui;
+}
+
+static inline void tui_windows_free(tui_window_t*** windows, size_t* count);
+
+/*
+ * Free parent window struct
+ */
+static inline void tui_window_parent_free(tui_window_parent_t** window)
+{
+  tui_windows_free(&(*window)->children, &(*window)->child_count);
+
+  tui_ncurses_window_free(&(*window)->head.window);
+
+  free(*window);
+
+  *window = NULL;
+}
+
+/*
+ * Free text window struct
+ */
+static inline void tui_window_text_free(tui_window_text_t** window)
+{
+  tui_ncurses_window_free(&(*window)->head.window);
+
+  free((*window)->text);
+
+  free(*window);
+
+  *window = NULL;
+}
+
+/*
+ * Free input window struct
+ */
+static inline void tui_window_input_free(tui_window_input_t** window)
+{
+  tui_ncurses_window_free(&(*window)->head.window);
+
+  free(*window);
+
+  *window = NULL;
+}
+
+/*
+ * Free window struct
+ */
+static inline void tui_window_free(tui_window_t** window)
+{
+  if (!window || !(*window)) return;
+
+  switch ((*window)->type)
+  {
+    case TUI_WINDOW_PARENT:
+      tui_window_parent_free((tui_window_parent_t**) window);
+      break;
+
+    case TUI_WINDOW_TEXT:
+      tui_window_text_free((tui_window_text_t**) window);
+      break;
+
+    case TUI_WINDOW_INPUT:
+      tui_window_input_free((tui_window_input_t**) window);
+      break;
+
+    default:
+      break;
+  }
+}
+
+/*
+ * Free windows
+ */
+static inline void tui_windows_free(tui_window_t*** windows, size_t* count)
+{
+  for (size_t index = 0; index < *count; index++)
+  {
+    tui_window_free(&(*windows)[index]);
+  }
+
+  free(*windows);
+
+  *windows = NULL;
+  *count = 0;
+}
+
+/*
+ * Free menu struct
+ */
+static inline void tui_menu_free(tui_menu_t** menu)
+{
+  if (!menu || !(*menu)) return;
+
+  tui_windows_free(&(*menu)->windows, &(*menu)->window_count);
+
+  free(*menu);
+
+  *menu = NULL;
+}
+
+/*
+ * Delete tui struct
+ */
+void tui_delete(tui_t** tui)
+{
+  if (!tui || !(*tui)) return;
+
+  for (size_t index = 0; index < (*tui)->menu_count; index++)
+  {
+    tui_menu_free(&(*tui)->menus[index]);
+  }
+
+  free((*tui)->menus);
+
+  tui_windows_free(&(*tui)->windows, &(*tui)->window_count);
+
+  free((*tui)->tab_windows);
+
+  free(*tui);
+
+  *tui = NULL;
+}
+
+/*
+ * Trigger tui events
+ */
+void tui_event(tui_t* tui, int key)
+{
+
+}
+
+void tui_render(tui_t* tui)
+{
+  curs_set(0);
+
+  refresh();
 }
 
 #endif // TUI_IMPLEMENT
